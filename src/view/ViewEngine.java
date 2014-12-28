2143f7d367238a8cc0c57b0d8bde4938a678c241
nikoszoisse <nikoszoisse@gmail.com>
2014-12-28 15:46:48 +0200

    Starting ViewEngine (Prototype of report)

--- src/view/ViewEngine.java
+++ src/view/ViewEngine.java
@@ -1,5 +1,50 @@
 package view;
 
+import application.AppManager;
+
 public class ViewEngine {
+	
+	/*Methods*/
+	ViewEngine(AppManager running_app){
+		
+	}
+	
+	/*Interact with GUI routines*/
+	public void addTab(){
+		
+	}
+	
+	public void closeTab(){
+		
+	}
+	
+	public void createSaveFileDialog(){
+		
+	}
+	
+	public void createReportFileDialog(){
+		
+	}
+	
+	public void viewLaw(){
+		
+	}
+	
+	public void exportReportEvent(String file_path){
+		
+	}
+	
+	public void parseFileEvent(String file_path){
+		
+	}
+	
+	/*Required running methods of view Engine */
+	public void init(){
+		
+	}
+	
+	public void close(){
+		
+	}
 
 }
