9684484630453e0d3f46bf2f47c1d3e19cab4982
aster <masterios@windowslive.com>
2014-12-28 19:30:10 +0200

    Τελικο σταδιο Version πιστευω..λιγο απο Law

--- src/application/Law.java
+++ src/application/Law.java
@@ -1,5 +1,41 @@
 package application;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Law {
-
+	private int lawId;
+	private String labelX,labelY;
+	private String comment;
+	private boolean accepted;
+	float[] valuesX,valuesY;
+	ArrayList<Version> versions;
+	
+	public Law(String xLabel,String yLabel,float[] xValues,float[] yValues,
+			int lawId,ArrayList versions){
+		this.labelX=xLabel;
+		this.labelY=yLabel;
+		this.lawId=lawId;
+		this.valuesX=xValues;
+		this.valuesY=yValues;
+		this.versions=versions;
+		
+	}
+	
+	public void checkLaw(){
+		if(lawId==1){
+			
+		}
+	}
+	
+	
+	public void setComment(String userComment){
+		this.comment=userComment;
+	}
+	public String getComment(){
+		return comment;
+	}
+	
+	public boolean checkConfirmation(){
+		return accepted;
+	}
 }
