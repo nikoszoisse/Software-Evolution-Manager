6a697a6f161cbdfd665502f326c2d4d77faa49cd
nikoszoisse <nikoszoisse@gmail.com>
2014-12-29 16:42:48 +0200

    showErrorDialog() Added, Use this only from AppManager
    e.x if parser failed, Parser Controller will handle this message and
    sent it to AppManager that parse failed. Use String Error Parameter

--- src/view/ViewEngine.java
+++ src/view/ViewEngine.java
@@ -7,24 +7,21 @@ import javax.swing.JMenuBar;
 
 import application.AppManager;
 
+import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JTabbedPane;
-import javax.swing.SwingConstants;
 
 import java.awt.BorderLayout;
 
 import javax.swing.JPanel;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 
 public class ViewEngine {
@@ -194,6 +191,18 @@ public class ViewEngine {
 
 	public void close(){
 		
+	}
+	
+	/**
+	 * Creates an error dialog
+	 * can called by AppManager
+	 * @param message
+	 */
+	public void showErrorDialog(String message){
+		JOptionPane.showMessageDialog(frame,
+			    message,
+			    "Opps error",
+			    JOptionPane.ERROR_MESSAGE);
 	}
 	
 	/**
